14df0fdf60e09347e785d34de03da6e2fc823764
7738309364 <125154572+7738309364@users.noreply.github.com>
2025-06-27 09:27:57 +0530

    Add files via upload

---
+++ User/Scripts/userscript.js
@@ -0,0 +1,10 @@
+﻿// Toggle submenu visibility
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll('.has-sub > a').forEach(function (item) {
+        item.addEventListener('click', function (e) {
+            e.preventDefault();
+            let parent = this.parentElement;
+            parent.classList.toggle("active");
+        });
+    });
+});
